0ba472ac0a2a77bc8384471f9e1c4298d2fb3f7c
Kimtho6 <kt@heyday.dk>
2021-03-08 12:54:40 +0100

    remove ASIdentifierManager

--- src/ios/IonicDeeplinkPlugin.m
+++ src/ios/IonicDeeplinkPlugin.m
@@ -123,23 +123,6 @@
 - (void)getHardwareInfo:(CDVInvokedUrlCommand *)command {
   NSMutableDictionary *info = [[NSMutableDictionary alloc] init];
 
-  // Get the as id in a way that doesn't require it be linked
-  Class asIdManClass = NSClassFromString(@"ASIdentifierManager");
-
-  if(asIdManClass) {
-    SEL sharedManagerSel = NSSelectorFromString(@"sharedManager");
-    id sharedManager = ((id (*)(id, SEL))[asIdManClass methodForSelector:sharedManagerSel])(asIdManClass, sharedManagerSel);
-    SEL advertisingIdentifierSelector = NSSelectorFromString(@"advertisingIdentifier");
-    NSUUID *uuid = ((NSUUID* (*)(id, SEL))[sharedManager methodForSelector:advertisingIdentifierSelector])(sharedManager, advertisingIdentifierSelector);
-    NSString *adId = [uuid UUIDString];
-
-    // Check if ad tracking is disabled (happens on iOS 10+)
-    NSString *disabledString = @"00000000-0000-0000-0000-000000000000";
-    if (![adId isEqualToString:disabledString]) {
-      [info setObject:adId forKey:@"adid"];
-    }
-  }
-
   NSString *uuid = [[UIDevice currentDevice].identifierForVendor UUIDString];
 
   if(uuid && [uuid length] > 0) {
